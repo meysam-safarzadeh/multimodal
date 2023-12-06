245d23e26c5d67f29ddaf6ea457f1e7501f137e2
meysam <m.s.97.eng@gmail.com>
2023-12-05 20:45:41 -0500

    Implemented hyperparameter tuning using Optuna for main function
    Enhanced the `main` function to support dynamic hyperparameter optimization with Optuna. This includes defining a search space for various parameters like `hidden_dim`, `num_heads`, `num_layers`, `learning_rate`, `dropout_rate`, `weight_decay`, `downsample_method`, `mode`, `fusion_layers`, `n_bottlenecks`, and `batch_size`. Integrated Optuna's `objective` function to suggest hyperparameters and maximize validation accuracy. The script runs optimization trials to identify the best combination of parameters for improved model performance.

---
+++ hyperparameter_tuning.py
@@ -0,0 +1,51 @@
+import optuna
+import torch
+from train import main
+
+
+def objective(trial):
+    # Hyperparameters to be tuned
+    hidden_dim = [
+        trial.suggest_int("hidden_dim_1", 64, 128, step=32),
+        trial.suggest_int("hidden_dim_2", 512, 2048, step=256),
+        trial.suggest_int("hidden_dim_3", 128, 512, step=64)
+    ]
+    num_heads = [
+        trial.suggest_categorical("num_heads_1", [2, 11]),
+        trial.suggest_categorical("num_heads_2", [8, 16, 32, 64]),
+        trial.suggest_categorical("num_heads_3", [2, 11])
+    ]
+    num_layers = [
+        trial.suggest_int("num_layers_1", 1, 6),
+        trial.suggest_int("num_layers_2", 1, 6)
+    ]
+    learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-3)
+    dropout_rate = trial.suggest_float("dropout_rate", 0.0, 0.2)
+    weight_decay = trial.suggest_float("weight_decay", 1e-5, 1e-3)
+    downsample_method = trial.suggest_categorical("downsample_method", ['Linear', 'MaxPool'])
+    mode = trial.suggest_categorical("mode", ['concat', 'separate'])
+    fusion_layers = trial.suggest_int("fusion_layers", 2, 6)
+    n_bottlenecks = trial.suggest_int("n_bottlenecks", 3, 7)
+    batch_size = trial.suggest_categorical("batch_size", [32, 64, 128])
+
+    # Calling the main function with the suggested hyperparameters
+    _, _, _, _, best_val_acc = main(hidden_dim, num_heads, num_layers, learning_rate,
+                                    dropout_rate, weight_decay, downsample_method, mode, fusion_layers,
+                                    n_bottlenecks, batch_size, num_epochs=2, verbose=False, fold=1, device='cuda:1',
+                                    save_model=False)
+
+    # Optuna aims to maximize the returned value
+    return best_val_acc
+
+
+# Running the optimization
+study = optuna.create_study(direction="maximize")
+study.optimize(objective, n_trials=100, n_jobs=12, show_progress_bar=True)  # Adjust the number of trials as needed
+
+print("Best trial:")
+trial = study.best_trial
+
+print(" Value: ", trial.value)
+print(" Params: ")
+for key, value in trial.params.items():
+    print(f"    {key}: {value}")
