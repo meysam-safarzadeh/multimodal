e5dbf37568f74bd3cfe60cac188c1dd3b19e005b
meysam <m.s.97.eng@gmail.com>
2023-12-27 13:48:56 -0500

    Implemented PCA analysis on fau embeddings
    PCA reduction reduces the dimensions to two and plots the data in new domain

--- FAU_embedding/dataset_analysis.py
+++ FAU_embedding/dataset_analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 import umap
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 
 # Load the dataset
@@ -32,6 +33,21 @@ for label in np.unique(y):
 plt.title('UMAP projection of the dataset')
 plt.xlabel('UMAP 1')
 plt.ylabel('UMAP 2')
-plt.legend('upper right', title='Label')
+plt.legend(title='Label')
 plt.show()
 
+# Applying PCA for dimensionality reduction
+pca_model = PCA(n_components=2)
+X_reduced_pca = pca_model.fit_transform(X)
+
+# Plotting PCA results
+plt.figure(figsize=(10, 8))
+for label in np.unique(y):
+    indices = np.where(y == label)
+    plt.scatter(X_reduced_pca[indices, 0], X_reduced_pca[indices, 1], label=label, alpha=0.5)
+
+plt.title('PCA projection of the dataset')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend(title='Label')
+plt.show()
